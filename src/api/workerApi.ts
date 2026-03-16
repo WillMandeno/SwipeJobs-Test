8c5d6a9b24fb7a3ce6bb1cfd1233fdd3236f40bf
Will Mandeno <willmandeno@Wills-MacBook-Pro.local>
2026-03-16 15:58:24 +1100

    add matches API service

--- src/api/workerApi.ts
+++ src/api/workerApi.ts
@@ -1,9 +1,16 @@
 import axios from 'axios'
+import type { Job } from "../types/jobs.ts"
 
 const API_BASE = 'https://test.swipejobs.com/'
+const WORKER_ID = '7f90df6e-b832-44e2-b624-3143d428001f'
 
 const api = axios.create({
     baseURL: API_BASE,
 })
+
+export async function getJobs(): Promise<Job[]> {
+    const response = await api.get(`worker/${WORKER_ID}/matches`)
+    return response.data
+}
 
 export default api
