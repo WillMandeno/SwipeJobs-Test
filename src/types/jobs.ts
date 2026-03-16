bc33671b0ec6d839be771d02bb0e989c88dc2572
Will Mandeno <willmandeno@Wills-MacBook-Pro.local>
2026-03-16 15:53:10 +1100

    types for job data

---
+++ src/types/jobs.ts
@@ -0,0 +1,37 @@
+export interface Job {
+  jobId: string
+  jobTitle: JobTitle
+  company: Company
+  wagePerHourInCents: number
+  milesToTravel: number
+  shifts: Shift[]
+  branch: string
+  branchPhoneNumber: string
+  requirements?: string[]
+}
+
+export interface JobTitle {
+  name: string
+  imageUrl: string
+}
+
+export interface Company {
+  name: string
+  address: Address
+  reportTo: ReportTo
+}
+
+export interface Address {
+  formattedAddress: string
+  zoneId: string
+}
+
+export interface ReportTo {
+  name: string
+  phone?: string
+}
+
+export interface Shift {
+  startDate: string
+  endDate: string
+}
