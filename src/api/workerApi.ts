b59cb8dbbee8cc51a9d0a13fc92e129c55901185
Will Mandeno <willmandeno@Wills-MacBook-Pro.local>
2026-03-16 16:09:05 +1100

    fix: incorrect api path

--- src/api/workerApi.ts
+++ src/api/workerApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import type { Job } from "../types/jobs.ts"
 
-const API_BASE = 'https://test.swipejobs.com/'
+const API_BASE = 'https://test.swipejobs.com/api/'
 const WORKER_ID = '7f90df6e-b832-44e2-b624-3143d428001f'
 
 const api = axios.create({
